918847404715799c86858cf0b9371b3e32293539
Ondřej Čertík <ondrej.certik@gmail.com>
2015-11-05 12:17:03 -0700

    Add a build line into build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 g++ -O3 -std=c++11 -o flint flint.cpp -lflint -lstdc++ -lpthread
 g++ -O3 -std=c++11 piranha.cpp -lgmp -lmpfr -lpthread
+gfortran -O3 -march=native -ffast-math -funroll-loops -o double double.f90
